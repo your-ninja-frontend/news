b1da35a21fd1e4aa6571bfd7c405ff8158ec21db
ninja_frontend <stifffffler102@gmail.com>
2024-11-24 23:49:02 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <div className=''></div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  }
+});
