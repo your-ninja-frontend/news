11772962bf6be7447656246c9ffcf6b36d0826cd
ninja_frontend <stifffffler102@gmail.com>
2024-11-27 23:13:25 +0300

    feat: add base font rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Play', 'ui-sans-serif', 'system-ui', 'sans-serif']
+    },
     extend: {}
   },
   plugins: []
