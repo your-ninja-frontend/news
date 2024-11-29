af4fbba141c8c6f1f67c90e1bae8dfdefabce8c2
ninja_frontend <stifffffler102@gmail.com>
2024-11-29 17:00:59 +0300

    fix: fix style priority

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  important: true,
   theme: {
     fontFamily: {
       sans: ['Play', 'ui-sans-serif', 'system-ui', 'sans-serif']
