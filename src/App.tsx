1cd1d1ed2bb814829ffd68bda12d67d59c6dc6fd
ninja_frontend <stifffffler102@gmail.com>
2024-12-19 21:25:45 +0300

    feat: add base routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { Layout, Typography } from 'antd';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './App.scss';
-import Header from './components/Header';
+import Nav from './components/Nav';
+import Home from './pages/Home/Home';
+import News from './pages/News/News';
+import NotFound from './pages/NotFound/NotFound';
+
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <BrowserRouter>
+      <Layout className='font-sans min-h-full'>
+        <Header className='flex items-center bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300'>
+          <Title className='text-xs mb-0'>Hacker News</Title>
+          <Nav />
+        </Header>
+        <Content className='grow'>
+          <Routes>
+            <Route index element={<Home />}></Route>
+            <Route path='news' element={<News />}></Route>
+            <Route path='*' element={<NotFound />}></Route>
+          </Routes>
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
